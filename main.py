fd18a53190f38620c180c576fa778761f4e1b04c
MikhailKochetkov <consultant.dax@gmail.com>
2024-02-26 22:05:04 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import random
 
+from settings import SKIP_ROWS
+
 
 def shuffle_lists(lists: list[list]) -> list:
-    shuffled_lists = [lst.copy() for lst in lists]
-    for i in range(len(shuffled_lists)):
-        random.shuffle(shuffled_lists[i])
-    while any(shuffled_lists[0][i] == shuffled_lists[1][i] or shuffled_lists[0][i] == shuffled_lists[2][i] or
-              shuffled_lists[1][i] == shuffled_lists[2][i] for i in range(len(shuffled_lists[0]))):
-        for i in range(len(shuffled_lists)):
-            random.shuffle(shuffled_lists[i])
-    return shuffled_lists
+    copy_lists = [lst.copy() for lst in lists]
+    for i in range(len(copy_lists)):
+        random.shuffle(copy_lists[i])
+    while any(copy_lists[0][i] == copy_lists[1][i] or
+              copy_lists[0][i] == copy_lists[2][i] or
+              copy_lists[1][i] == copy_lists[2][i]
+              for i in range(len(copy_lists[0]))):
+        for i in range(len(copy_lists)):
+            random.shuffle(copy_lists[i])
+    return copy_lists
 
 
 def repeat_element(lst: list, counts: list) -> list:
@@ -21,7 +25,7 @@ def repeat_element(lst: list, counts: list) -> list:
 
 
 def main():
-    read_data = pd.read_excel('Book1.xlsx', skiprows=2)
+    read_data = pd.read_excel('Book1.xlsx', skiprows=SKIP_ROWS)
     names_list = read_data.iloc[:, 0].tolist()
     counts_list = read_data.columns.tolist()[1:]
     df_dict = {}
@@ -34,7 +38,10 @@ def main():
     for i in range(len(shuffled_lists)):
         df_dict[i+1] = shuffled_lists[i]
     df = pd.DataFrame(df_dict)
-    with pd.ExcelWriter('Book1.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+    with pd.ExcelWriter('Book1.xlsx',
+                        engine='openpyxl',
+                        mode='a',
+                        if_sheet_exists='replace') as writer:
         df.to_excel(writer, sheet_name='Sheet2', index=False)
 
 
---
+++ settings.py
@@ -0,0 +1,1 @@
+SKIP_ROWS = 2
